1ce71292467fbb8c1e51d6ab689200906384464b
cristiane <cris_csr@yahoo.com.br>
2019-03-13 13:11:07 -0300

    method setC call

--- src/rename/Rename.java
+++ src/rename/Rename.java
@@ -14,8 +14,12 @@ public class Rename {
     /**
      * @param args the command line arguments
      */
+    
+     
     public static void main(String[] args) {
         // TODO code application logic here
+       C ClassC = new C();
+       ClassC.setC(20);
     }
     
 }

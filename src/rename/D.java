a8cd05056dc12f71caf3d85d3840e746c6386be2
cristiane <cris_csr@yahoo.com.br>
2019-03-14 21:48:00 -0300

    classD created

---
+++ src/rename/D.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rename;
+
+/**
+ *
+ * @author Cristiane
+ */
+public class D {
+    int D;
+    String DD;
+    A ClassA;
+    
+    public void setD(int D){
+        this.D = D;
+        ClassA.setA(D);
+    }
+}

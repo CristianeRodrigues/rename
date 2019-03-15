9f52124d151b7eb1098f641d3d988f31cda9b8a2
cristiane <cris_csr@yahoo.com.br>
2019-03-14 21:53:34 -0300

    edição do método setA e criação do metodo getA

--- src/rename/A.java
+++ src/rename/A.java
@@ -13,7 +13,12 @@ public class A {
     int A;
     String AA;
     
+    public int getA(){
+        return A;
+    }
+    
     public void setA(int A){
+        System.out.println("classe A");
         this.A = A;
     }
     
